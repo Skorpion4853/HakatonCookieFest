f4a3543878a92b69fce1ef13179196796ddcac3d
ATLASO4EK <126068818+ATLASO4EK@users.noreply.github.com>
2024-11-30 14:33:47 +0700

    Add files via upload

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -4,9 +4,21 @@ import asyncio
 import sngs
 from telebot.async_telebot import AsyncTeleBot
 from telebot import types
+from telebot.asyncio_storage import StateMemoryStorage
+from telebot.asyncio_handler_backends import State, StatesGroup
+from telebot import asyncio_filters
+from telebot.states.sync.context import StateContext
 
 #Объяление бота
-bot = AsyncTeleBot(sngs.bot_token)
+bot = AsyncTeleBot(sngs.bot_token ,state_storage=StateMemoryStorage())
+
+class loginState(StatesGroup):
+    login = State()
+    password = State()
+    authorizated = State()
+
+def check_login(login, password):
+    return True
 
 #Подключение к SQL-серверу
 sql = mysql.connector.connect(user = sngs.USERNAME,
@@ -18,9 +30,38 @@ cursor = sql.cursor()
 
 #Реакция бота на команду /start
 @bot.message_handler(commands=['start'])
-async def start(message):
-    text = 'Привет!\nЯ - чат-бот по мотивации вашей работы!\nПожалуйста, введите ваш логин и пароль через двоеточие\nНапрмер: ivanov:12345'
+async def start(message, state: StateContext):
+    text = 'Привет!\nЯ - чат-бот по мотивации вашей работы!\nПожалуйста, введите ваш логин'
     await bot.send_message(message.chat.id, text)
+    await bot.set_state(user_id=message.from_user.id, state=loginState.login,
+                          chat_id=message.chat.id)
+
+# Здесь пользователь вводит логин свой
+@bot.message_handler(state=loginState.login)
+async def login_state(message, state: StateContext):
+    #Устанавливаем стейт
+    await state.add_data(login=message.text)
+
+    await bot.send_message(message.chat.id, "Введите пароль")
+
+    await bot.set_state(user_id=message.from_user.id, state=loginState.password,
+                          chat_id=message.chat.id)
+
+# Здесь пользователь вводит свой пароль
+@bot.message_handler(state=loginState.password)
+async def password_state(message, state: StateContext):
+    await state.add_data(password=message.text)
+    async with state.data() as data:
+        login = data.get("login")
+        password = data.get("password")
+    status = check_login(login=login, password=password)
+    if status:
+        await bot.send_message(message.chat.id, f"Вы успешно зашли под логином: {login}")
+        await bot.set_state(user_id=message.from_user.id, state=loginState.authorizated,
+                          chat_id=message.chat.id)
+    else:
+        await bot.send_message(message.chat.id, f"Неправильный логин или пароль!!")
+    await bot.delete_state(user_id=message.from_user.id, chat_id=message.chat.id)
 
 
 #Main Menu
@@ -32,8 +73,8 @@ async def MainMenu(message):
         text = f'Привет, {username}\nВыбери действие'
         mkup = types.InlineKeyboardMarkup(row_width=1)
         rngs_btn = types.InlineKeyboardButton(text='Рейтинги \U0001F4C8', callback_data='ratingsEmployeer')
-        dwnld_btn = types.InlineKeyboardButton(text='Выгрузить БД', callback_data='downloadDB')
-        mng_btn = types.InlineKeyboardButton(text='Управление сотрудниками', callback_data='Manage')
+        dwnld_btn = types.InlineKeyboardButton(text='Выгрузить БД \U0001F4E5', callback_data='downloadDB')
+        mng_btn = types.InlineKeyboardButton(text='Управление сотрудниками \U0001F9FE', callback_data='Manage')
         mkup.add(rngs_btn, dwnld_btn, mng_btn)
         await bot.send_message(message.chat.id, text, reply_markup=mkup)
     elif adm == 0:      #надо брать из БД, но пока заглушка
@@ -88,8 +129,8 @@ async def check_callback_data(callback):
         text = f'Привет, {username}\nВыбери действие'
         mkup = types.InlineKeyboardMarkup(row_width=1)
         rngs_btn = types.InlineKeyboardButton(text='Рейтинги \U0001F4C8', callback_data='ratingsEmployeer')
-        dwnld_btn = types.InlineKeyboardButton(text='Выгрузить БД', callback_data='downloadDB')
-        mng_btn = types.InlineKeyboardButton(text='Управление сотрудниками', callback_data='Manage')
+        dwnld_btn = types.InlineKeyboardButton(text='Выгрузить БД \U0001F4E5', callback_data='downloadDB')
+        mng_btn = types.InlineKeyboardButton(text='Управление сотрудниками \U0001F9FE', callback_data='Manage')
         mkup.add(rngs_btn, dwnld_btn, mng_btn)
         await bot.edit_message_text(text, callback.message.chat.id, callback.message.id, reply_markup=mkup)
     elif callback.data == 'downloadDB':
@@ -105,14 +146,18 @@ async def check_callback_data(callback):
     elif callback.data == 'Manage':
         text = 'Выберите действие'
         mkup = types.InlineKeyboardMarkup(row_width=1)
-        add_btn = types.InlineKeyboardButton(text = 'Добавить сотрудника', callback_data='addEmployee')
-        del_btn = types.InlineKeyboardButton(text = 'Удалить сотрудника', callback_data='delEmployee')
+        add_btn = types.InlineKeyboardButton(text = 'Добавить сотрудника \U00002714', callback_data='addEmployee')
+        del_btn = types.InlineKeyboardButton(text = 'Удалить сотрудника \U0000274C', callback_data='delEmployee')
         bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToEmployeerMenu')
         mkup.add(add_btn, del_btn, bck_btn)
         await bot.edit_message_text(text, callback.message.chat.id, callback.message.id, reply_markup=mkup)
+
+
 #Запуск бота
+bot.add_custom_filter(asyncio_filters.StateFilter(bot))
+from telebot.states.asyncio.middleware import StateMiddleware
+
+bot.setup_middleware(StateMiddleware(bot))
 asyncio.run(bot.polling())
-
-
 
 
