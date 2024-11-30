201264a8041c3ce431cc36a29c05bce23189efd2
ATLASO4EK <126068818+ATLASO4EK@users.noreply.github.com>
2024-11-30 17:52:50 +0700

    Add files via upload

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -75,11 +75,12 @@ async def MainMenu(message, state: StateContext):
         username = 'Иванов Иван Иваныч'     #надо брать из БД, но пока заглушка
         text = f'Привет, {username}\nВыбери действие'
         mkup = types.InlineKeyboardMarkup(row_width=1)
-        rngs_btn = types.InlineKeyboardButton(text='Рейтинги \U0001F4C8', callback_data='ratingsEmployeer')
+        rngs_btn = types.InlineKeyboardButton(text='Рейтинги \U0001F4C8', callback_data='ratings')
         dwnld_btn = types.InlineKeyboardButton(text='Выгрузить БД \U0001F4E5', callback_data='downloadDB')
         mng_btn = types.InlineKeyboardButton(text='Управление сотрудниками \U0001F9FE', callback_data='Manage')
         mkup.add(rngs_btn, dwnld_btn, mng_btn)
-        await bot.send_message(message.chat.id, text, reply_markup=mkup)
+        try: await bot.edit_message_text(text, message.chat.id, message.id, reply_markup=mkup)
+        except: await bot.send_message(message.chat.id, text, reply_markup=mkup)
     elif adm == 0:
         username = 'Иванов Иван Иваныч'     #надо брать из БД, но пока заглушка
         text = f'Привет, {username}\nВыбери действие'
@@ -87,75 +88,63 @@ async def MainMenu(message, state: StateContext):
         rngs_btn = types.InlineKeyboardButton(text = 'Рейтинги \U0001F4C8', callback_data='ratings')
         prfl_btn = types.InlineKeyboardButton(text = 'Профиль \U0000274C', callback_data='profile')
         mkup.add(rngs_btn,prfl_btn)
-        await bot.send_message(message.chat.id, text, reply_markup= mkup)
+        try:
+            await bot.edit_message_text(text, message.chat.id, message.id, reply_markup=mkup)
+        except:
+            await bot.send_message(message.chat.id, text, reply_markup=mkup)
 
 
 #Обработка callback ботом
-@bot.callback_query_handler(func = lambda callback: callback.data)
-async def check_callback_data(callback):
-    #callback'и работника
-    if callback.data == 'ratings': #Выводим раздел рейтинги
-        mkup = types.InlineKeyboardMarkup(row_width=1)
-        glb_rtngs_btn = types.InlineKeyboardButton(text='Глобальный \U0001F5FA', callback_data='global rating')
-        brnch_rtngs_btn = types.InlineKeyboardButton(text='Филиалов \U0001F3D8', callback_data='branches rating')
-        lcl_rtngs_btn = types.InlineKeyboardButton(text='Вашего филилала \U0001F3E0', callback_data='local rating')
-        bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToEmployeeMenu')
-        mkup.add(glb_rtngs_btn, brnch_rtngs_btn, lcl_rtngs_btn, bck_btn)
-        await bot.edit_message_text('Выберите рейтинг \U0001F4C8',callback.message.chat.id, callback.message.id , reply_markup = mkup)
-    elif callback.data == 'profile': #Выводим раздел профиль
-        username = 'Иванов Иван Иваныч'  # Заглушка, брать из БД
-        text = 'Данная функция пока в разработке \U0001F634'
-        mkup = types.InlineKeyboardMarkup()
-        bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToEmployeeMenu')
-        mkup.add(bck_btn)
-        await bot.delete_message(callback.message.chat.id, callback.message.id)
-        await bot.send_message(callback.message.chat.id, text, reply_markup=mkup)
-    elif callback.data == 'BackToEmployeeMenu': #Возврат к меню работника
-        username = 'Иванов Иван Иваныч' #Заглушка, брать из БД
-        text = f'Привет, {username}\nВыбери действие'
-        marup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton(text='Рейтинги \U0001F4C8', callback_data='ratings')
-        btn2 = types.InlineKeyboardButton(text='Профиль \U0000274C', callback_data='profile')
-        marup.add(btn1, btn2)
-        await bot.edit_message_text(text, callback.message.chat.id, callback.message.id, reply_markup=marup)
+#Меню рейтингов
+@bot.callback_query_handler(lambda c: c.data == 'ratings') #Рейтинг
+async def employee_rating(callback_query: types.CallbackQuery):
+    mkup = types.InlineKeyboardMarkup(row_width=1)
+    glb_rtngs_btn = types.InlineKeyboardButton(text='Глобальный \U0001F5FA', callback_data='global rating')
+    brnch_rtngs_btn = types.InlineKeyboardButton(text='Филиалов \U0001F3D8', callback_data='branches rating')
+    lcl_rtngs_btn = types.InlineKeyboardButton(text='Вашего филилала \U0001F3E0', callback_data='local rating')
+    bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToMenu')
+    mkup.add(glb_rtngs_btn, brnch_rtngs_btn, lcl_rtngs_btn, bck_btn)
+    await bot.edit_message_text('Выберите рейтинг \U0001F4C8', callback_query.message.chat.id, callback_query.message.id,
+                                reply_markup=mkup)
 
-    #callback'и работодателя
-    elif callback.data == 'ratingsEmployeer': #Выводим раздел рейтинги
-        mkup = types.InlineKeyboardMarkup(row_width=1)
-        glb_rtngs_btn = types.InlineKeyboardButton(text='Глобальный \U0001F5FA', callback_data='global rating em')
-        brnch_rtngs_btn = types.InlineKeyboardButton(text='Филиалов \U0001F3D8', callback_data='branches rating em')
-        lcl_rtngs_btn = types.InlineKeyboardButton(text='Вашего филилала \U0001F3E0', callback_data='local rating em')
-        bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToEmployeerMenu')
-        mkup.add(glb_rtngs_btn, brnch_rtngs_btn, lcl_rtngs_btn, bck_btn)
-        await bot.edit_message_text('Выберите рейтинг \U0001F4C8',callback.message.chat.id, callback.message.id , reply_markup = mkup)
-    elif callback.data == 'BackToEmployeerMenu':
-        username = 'Иванов Иван Иваныч'  # надо брать из БД, но пока заглушка
-        text = f'Привет, {username}\nВыбери действие'
-        mkup = types.InlineKeyboardMarkup(row_width=1)
-        rngs_btn = types.InlineKeyboardButton(text='Рейтинги \U0001F4C8', callback_data='ratingsEmployeer')
-        dwnld_btn = types.InlineKeyboardButton(text='Выгрузить БД \U0001F4E5', callback_data='downloadDB')
-        mng_btn = types.InlineKeyboardButton(text='Управление сотрудниками \U0001F9FE', callback_data='Manage')
-        mkup.add(rngs_btn, dwnld_btn, mng_btn)
-        await bot.edit_message_text(text, callback.message.chat.id, callback.message.id, reply_markup=mkup)
-    elif callback.data == 'downloadDB':
-        text = 'Выберите формат файла'
-        mkup = types.InlineKeyboardMarkup(row_width=1)
-        xlsx_btn = types.InlineKeyboardButton(text='Exel .xlsx', callback_data='dwnldXLSX')
-        json_btn = types.InlineKeyboardButton(text='JavaScript Object Notation .json', callback_data='dwnldJSON')
-        csv_btn = types.InlineKeyboardButton(text='Comma-Separated Values .csv', callback_data='dwnldCSV')
-        bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToEmployeerMenu')
-        mkup.add(xlsx_btn, json_btn, csv_btn, bck_btn)
-        await bot.delete_message(callback.message.chat.id, callback.message.id)
-        await bot.send_message(callback.message.chat.id, text, reply_markup=mkup)
-    elif callback.data == 'Manage':
-        text = 'Выберите действие'
-        mkup = types.InlineKeyboardMarkup(row_width=1)
-        add_btn = types.InlineKeyboardButton(text = 'Добавить сотрудника \U00002714', callback_data='addEmployee')
-        del_btn = types.InlineKeyboardButton(text = 'Удалить сотрудника \U0000274C', callback_data='delEmployee')
-        bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToEmployeerMenu')
-        mkup.add(add_btn, del_btn, bck_btn)
-        await bot.edit_message_text(text, callback.message.chat.id, callback.message.id, reply_markup=mkup)
+#Профиль
+@bot.callback_query_handler(lambda c: c.data == 'profile')
+async def employee_profile(callback: types.CallbackQuery):
+    text = 'Данная функция пока в разработке \U0001F634'
+    mkup = types.InlineKeyboardMarkup()
+    bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToMenu')
+    mkup.add(bck_btn)
+    await bot.delete_message(callback.message.chat.id, callback.message.id)
+    await bot.send_message(callback.message.chat.id, text, reply_markup=mkup)
 
+#Возврат в меню
+@bot.callback_query_handler(lambda c: c.data == "BackToMenu")
+async def back_menu(callback: types.CallbackQuery, state: StateContext):
+    await MainMenu(callback.message, state)
+
+#Скачать БД
+@bot.callback_query_handler(lambda c: c.data == 'downloadDB')
+async def download_DB(callback: types.CallbackQuery):
+    text = 'Выберите формат файла'
+    mkup = types.InlineKeyboardMarkup(row_width=1)
+    xlsx_btn = types.InlineKeyboardButton(text='Exel .xlsx', callback_data='dwnldXLSX')
+    json_btn = types.InlineKeyboardButton(text='JavaScript Object Notation .json', callback_data='dwnldJSON')
+    csv_btn = types.InlineKeyboardButton(text='Comma-Separated Values .csv', callback_data='dwnldCSV')
+    bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToMenu')
+    mkup.add(xlsx_btn, json_btn, csv_btn, bck_btn)
+    await bot.delete_message(callback.message.chat.id, callback.message.id)
+    await bot.send_message(callback.message.chat.id, text, reply_markup=mkup)
+
+#Управление сотрудниками
+@bot.callback_query_handler(lambda c:c.data == 'Manage')
+async def manage_employees(callback: types.CallbackQuery):
+    text = 'Выберите действие'
+    mkup = types.InlineKeyboardMarkup(row_width=1)
+    add_btn = types.InlineKeyboardButton(text='Добавить сотрудника \U00002714', callback_data='addEmployee')
+    del_btn = types.InlineKeyboardButton(text='Удалить сотрудника \U0000274C', callback_data='delEmployee')
+    bck_btn = types.InlineKeyboardButton(text='Назад', callback_data='BackToMenu')
+    mkup.add(add_btn, del_btn, bck_btn)
+    await bot.edit_message_text(text, callback.message.chat.id, callback.message.id, reply_markup=mkup)
 
 #Запуск бота
 bot.add_custom_filter(asyncio_filters.StateFilter(bot))
