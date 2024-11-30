85bad45228bf57fe7d852bc0527dd1d81eccb1a9
ATLASO4EK <126068818+ATLASO4EK@users.noreply.github.com>
2024-11-30 16:42:06 +0700

    Add files via upload

--- PythonProject/sngs.py
+++ PythonProject/sngs.py
@@ -9,13 +9,6 @@ SERVER = 'mysql-ru-br2.joinserver.xyz'
 DATABASE = 's252266_Hakaton'
 USERNAME = 'u252266_UemnEZfUmO'
 PASSWORD = 'Nn7EANEQ+M79lCxI=wgzR5Ya'
-connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
-
-#Основные SQL-запросы для удобства
-userID = -1;
-login = ''
-password = ''
-sqlAuth = f'SELECT * FROM Users Where login = {login} AND password = {password}' #Авторизация
 
 
 
