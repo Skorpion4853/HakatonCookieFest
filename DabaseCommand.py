a3efbeb151168ecd494febe3651c9dec8cf1cb46
Skorpion4853 <98880853+Skorpion4853@users.noreply.github.com>
2024-12-01 10:59:50 +0700

    add 2 new func
    func get_global_top need 2 option
       -login curr_worker
       -sorting (param to sort)
    func return_global_top need 1 option
       -sorting

--- DabaseCommand.py
+++ DabaseCommand.py
@@ -23,7 +23,7 @@ status_payment = ( #Для статусов оплаты были выбраны
     'Возврат'
 )
 
-def get_worker_top(cur_user: str, sorting: str) -> str: #Функция для вывода топ 10 работников
+def get_worker_top(cur_user: str, sorting: str) -> str: #Функция для вывода топ 10 работников филиала
 
     cnx = connect_to_mysql(get_config(), attempts=3) #делаем коннект к БД
 
@@ -109,7 +109,7 @@ def get_worker_top(cur_user: str, sorting: str) -> str: #Функция для �
         return "Could not connect"
 
 
-def return_worker_top(cur_user: str, sorting: str) -> pd.DataFrame or str: #Функция для выгрузки топа работников
+def return_worker_top(cur_user: str, sorting: str) -> pd.DataFrame or str: #Функция для выгрузки топа работников филиала
 
     cnx = connect_to_mysql(get_config(), attempts=3) #делаем коннект к БД
 
@@ -411,3 +411,140 @@ def auth(login: str, password: str) -> bool or None:
     else:
         return "Could not connect"
 
+def get_global_top(cur_user: str, sorting: str) -> str: #Функция для вывода топ 10 работников
+
+    cnx = connect_to_mysql(get_config(), attempts=3) #делаем коннект к БД
+
+    if cnx and cnx.is_connected(): #Если коннект прошел успешно создаем курсор
+        cursor = cnx.cursor()
+        global prices
+
+        #Вытаскивание данных из БД
+
+        #Запрос на вывод филиала залогиненого сотрудника
+        query = "SELECT id FROM Users WHERE login = %s"
+        cursor.execute(query, [cur_user])
+        cur_user = cursor.fetchall()
+        cur_user = cur_user[0][0]
+
+        #Запрос на вывод всех сотрудников филиала
+        query = "SELECT id, full_name FROM Users"
+        cursor.execute(query)
+        users = cursor.fetchall()
+
+        #Создание списка всех сотрудников филиала
+        users_id = []
+        for user in users:
+            users_id.append(user[0])
+
+        #Запрос на вывод всех операций сотрудников данного филиала
+        format_strings = ','.join(['%s'] * len(users_id))
+        cursor.execute("SELECT * FROM Operations WHERE worker_id IN (%s)" %format_strings,
+                       tuple(users_id))
+        operations = cursor.fetchall()
+
+        #Сохранение данных из БД в формате DataFrame для работы с ними
+        users_df = pd.DataFrame(columns=["id", "full_name"], data=users)
+        operation_df = pd.DataFrame(columns=["id", "counterparty", "worker_id", "type", "status", "status_payment",
+                                             "date", "price"], data=operations)
+
+        user_full_name = users_df[users_df["id"] == cur_user]['full_name'] #Сохранение ФИО пользователя
+        data = [] #Пустышка для сохранения данных для итогового, дата фрейма с рейтингом
+        for user in users_id: #перебираем всех юзеров, что работали в этом месяце
+            price = 0.00
+            salary = 0.00
+            for operation in operation_df[operation_df["worker_id"].isin([user])].to_numpy(): #Создаем объект дата фрейма со всеми операциями пользователя
+                price += operation[-1]
+                salary += operation[-1] * prices[operation[3]] / 100
+            full_name = users_df[users_df["id"].isin([user])]["full_name"].to_numpy()[0] #Вытаскиваем фио сотрудника
+            data.append([full_name, round(price, 2), round(salary, 2)])
+
+
+
+        df_top_u = pd.DataFrame(columns=["full_name", "price", "salary"], data=data) #Создаем pd серию для вывода
+        df_top_u = df_top_u.sort_values(sorting, ascending=False) #Делаем сортировку по выбранному параметру
+
+        user_place = -1 #Сохранение позиции в топе у user
+        place = 0 #Место в топе
+        top_str = "" #Строка для возврата рейтинга
+
+        for top_user in df_top_u.to_numpy():
+            place += 1 #Делаем перемещение по местам в топе
+            if user_full_name.to_numpy()[0] == top_user[0]: #Сохраняем позицию пользователя если ФИО совпадает
+                user_place = place
+
+            #Делаем проверку для первых 3 пользователей и добавляем им стикеры соответсвующее их месту в топе
+            if place == 1:
+                top_str += "\U0001F947 " + " ".join(map(str,top_user)) + "\n"
+            elif place == 2:
+                top_str += "\U0001F948 " + " ".join(map(str,top_user)) + "\n"
+            elif place == 3:
+                top_str += "\U0001F949 "+ " ".join(map(str,top_user))+ "\n"
+            elif place <= 10:
+                #Выводим оставшейся места с указанием их позиции в топе
+                top_str += str(place)+ " " + " ".join(map(str,top_user))+ "\n"
+            elif user_place > 10 and user_full_name.to_numpy()[0] == top_user[0]:
+                #Если пользователь не входит в 10 лучших, то выводим его место отдельно от остальных
+                top_str += ". . .\n"
+                top_str += str(place)+ " " + " ".join(map(str,top_user))+ "\n"
+
+        cursor.close()
+        cnx.close()
+
+        return top_str
+    else:
+        return "Could not connect"
+
+
+def return_global_top(sorting: str) -> pd.DataFrame or str: #Функция для вывода топа работников
+
+    cnx = connect_to_mysql(get_config(), attempts=3) #делаем коннект к БД
+
+    if cnx and cnx.is_connected(): #Если коннект прошел успешно создаем курсор
+        cursor = cnx.cursor()
+        global prices
+
+        #Вытаскивание данных из БД
+
+        #Запрос на вывод всех сотрудников филиала
+        query = "SELECT id, full_name FROM Users"
+        cursor.execute(query)
+        users = cursor.fetchall()
+
+        #Создание списка всех сотрудников филиала
+        users_id = []
+        for user in users:
+            users_id.append(user[0])
+
+        #Запрос на вывод всех операций сотрудников данного филиала
+        format_strings = ','.join(['%s'] * len(users_id))
+        cursor.execute("SELECT * FROM Operations WHERE worker_id IN (%s)" %format_strings,
+                       tuple(users_id))
+        operations = cursor.fetchall()
+
+        #Сохранение данных из БД в формате DataFrame для работы с ними
+        users_df = pd.DataFrame(columns=["id", "full_name"], data=users)
+        operation_df = pd.DataFrame(columns=["id", "counterparty", "worker_id", "type", "status", "status_payment",
+                                             "date", "price"], data=operations)
+
+        data = [] #Пустышка для сохранения данных для итогового, дата фрейма с рейтингом
+        for user in users_id: #перебираем всех юзеров, что работали в этом месяце
+            price = 0.00
+            salary = 0.00
+            for operation in operation_df[operation_df["worker_id"].isin([user])].to_numpy(): #Создаем объект дата фрейма со всеми операциями пользователя
+                price += operation[-1]
+                salary += operation[-1] * prices[operation[3]] / 100
+            full_name = users_df[users_df["id"].isin([user])]["full_name"].to_numpy()[0] #Вытаскиваем фио сотрудника
+            data.append([full_name, round(price, 2), round(salary, 2)])
+
+
+
+        df_top_u = pd.DataFrame(columns=["full_name", "price", "salary"], data=data) #Создаем pd серию для вывода
+        df_top_u = df_top_u.sort_values(sorting, ascending=False) #Делаем сортировку по выбранному параметру
+
+        cursor.close()
+        cnx.close()
+
+        return df_top_u
+    else:
+        return "Could not connect"
